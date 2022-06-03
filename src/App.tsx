429f08f60f486e6fa5835dbcf917986c5428f7b9
Tyler Renelle <tylerrenelle@gmail.com>
2022-06-03 12:30:14 -0700

    add drawer & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Drawer from './Drawer'
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import About from './logged_out/About'
+import Notes from './logged_out/Notes'
+import Home from './logged_out/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BrowserRouter>
+    <CssBaseline />
+    <Drawer>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="about/notes" element={<Notes />} />
+      </Routes>
+    </Drawer>
+  </BrowserRouter>
 }
 
 export default App;
---
+++ src/Drawer/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Wrapper from './Wrapper'
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import LoginIcon from '@mui/icons-material/Login';
+
+interface Props {
+  children: React.ReactElement
+}
+
+export default function Index(
+  {children}: Props
+) {
+  const title = "DIY Cloud Gaming"
+  const appBarLinks = [
+    <Button href='/login' color="inherit">Login</Button>,
+    <Button href='/about' color="inherit">About</Button>
+  ]
+  const sideBarLinks = [
+    {text: "About", href: "/about", icon: null},
+    {text: "Notes", href: "/about/notes", icon: null},
+  ].map(({text, href, icon}, index) => (
+    <ListItem key={text} disablePadding>
+      <ListItemButton href={href}>
+        <ListItemIcon>
+          {icon}
+        </ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItemButton>
+    </ListItem>
+  ))
+
+  return <Wrapper
+    title={title}
+    sideBarLinks={sideBarLinks}
+    appBarLinks={appBarLinks}
+  >
+    {children}
+  </Wrapper>
+}
