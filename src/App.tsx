e836942909828db58f8c152e86b49856b99e5e27
Tyler Renelle <tylerrenelle@gmail.com>
2022-06-07 00:24:00 -0700

    first pass at createInstances

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Drawer from './Drawer'
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Link } from "react-router-dom";
-import About from './logged_out/About'
-import Notes from './logged_out/Notes'
-import Home from './logged_out/Home'
+import About from './loggedOut/About'
+import Notes from './loggedOut/Notes'
+import Home from './loggedOut/Home'
+import Instances from './loggedIn/Instances'
+import Account from './loggedIn/Account'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return <BrowserRouter>
     <CssBaseline />
-    <Drawer>
+    <Drawer loggedIn={loggedIn} setLoggedIn={setLoggedIn}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="about/notes" element={<Notes />} />
+        {loggedIn ? <>
+          <Route path="/" element={<Instances />} />
+          <Route path="account" element={<Account />} />
+        </> : <>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="about/notes" element={<Notes />} />
+        </>}
       </Routes>
     </Drawer>
   </BrowserRouter>
